5b8da93e59142045073c439d9ed39e60412c859b
Joel <joel.carrizo.91@gmail.com>
2019-06-21 18:09:25 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hola_mundo_CA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo");
+            
+            Console.Read();
+        }
+    }
+}
